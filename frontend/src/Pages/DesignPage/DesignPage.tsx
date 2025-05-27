58840f7b1678f5f40a9827f1b10a8213de849921
CodexPremiera <ashleyken.professional@gmail.com>
2025-05-27 23:17:55 +0800

    add ratio list

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import Table from "../../Components/Table/Table";
+import RatioList from "../../Components/RatioList/RatioList";
 
 type Props = {};
 
@@ -11,6 +12,7 @@ const DesignPage = (props: Props) => {
         Design guide- This is the design guide for Fin Shark. These are reuable
         components of the app with brief instructions on how to use them.
       </h1>
+      <RatioList />
       <Table />
       <h3>
         Table - Table takes in a configuration object and company data as
