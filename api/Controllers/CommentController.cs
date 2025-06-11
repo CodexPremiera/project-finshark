00d124a17e59dd344765806a273458db59c1bfe3
CodexPremiera <ashleyken.professional@gmail.com>
2025-06-12 00:40:25 +0800

    get comments of stock

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -24,4 +24,15 @@ public class CommentController : ControllerBase
         
         return Ok(comments);
     }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var comment = await _commentRepo.GetByIdAsync(id);
+
+        if (comment == null)
+            return NotFound();
+
+        return Ok(comment.ToCommentDto());
+    }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ed247a5f45d21ae3b1064e9d40fbc7cda2762a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080dcf66b86c52c1dd2e9b2b676a99d3654b3c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
